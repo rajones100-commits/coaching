c31b8751ed32f6f3c35d94035a8b97e0ce255168
rajones100-commits <rajones100@gmail.com>
2026-09-08 01:18:31 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.COACHING_CONFIG = {
-  csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQHb-shuUGHiy-bitS8P9GHNul0EhPC0ohu1FOeFXPlEA10hcEMYkCY4yStnrbYRNVDDSDZw2eh98Vl/pub?output=csv",
-  currency: "GBP",
-  locale: "en-GB"
-};
